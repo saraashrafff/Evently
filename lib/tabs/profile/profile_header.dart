71060e950f8dfc7470a7c217f5c0b82d57e737c8
Sara <saraashraf55530@gmail.com>
2025-07-23 13:26:19 +0300

    Implement profile tab UI

---
+++ lib/tabs/profile/profile_header.dart
@@ -0,0 +1,35 @@
+import 'package:evently/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class ProfileHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: AppTheme.primary,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(64)),
+      ),
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset('assets/images/route_logo.png'),
+            SizedBox(width: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('User Name', style: textTheme.headlineSmall),
+                SizedBox(height: 10),
+                Text(
+                  'userEmail@gmail.com',
+                  style: textTheme.titleMedium!.copyWith(color: AppTheme.white),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/tabs/profile/profile_tab.dart
+++ lib/tabs/profile/profile_tab.dart
@@ -1,10 +1,94 @@
+import 'package:evently/app_theme.dart';
+import 'package:evently/tabs/profile/profile_header.dart';
 import 'package:flutter/material.dart';
 
 class ProfileTab extends StatelessWidget {
-  const ProfileTab({super.key});
-
+  List<Language> languages = [
+    Language(code: 'en', name: 'English'),
+    Language(code: 'ar', name: 'العربية'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Profile'));
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ProfileHeader(),
+        SizedBox(height: 24),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Language',
+                    style: textTheme.titleLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: AppTheme.black,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(color: AppTheme.primary),
+                    ),
+                    child: DropdownButton(
+                      value: 'en',
+                      iconEnabledColor: AppTheme.primary,
+                      underline: SizedBox(),
+                      borderRadius: BorderRadius.circular(16),
+                      items: languages
+                          .map(
+                            (language) => DropdownMenuItem(
+                              value: language.code,
+                              child: Text(
+                                language.name,
+                                style: textTheme.titleLarge!.copyWith(
+                                  color: AppTheme.primary,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Dark Theme',
+                    style: textTheme.titleLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: AppTheme.black,
+                    ),
+                  ),
+                  Switch(
+                    value: true,
+                    onChanged: (value) {},
+                    activeTrackColor: AppTheme.primary,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
+}
+
+class Language {
+  String code;
+  String name;
+  Language({required this.code, required this.name});
 }
